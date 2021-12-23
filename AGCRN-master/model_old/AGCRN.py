a6f9112ebb2739a509f06292d4c53e82ed822d04
jc <2428791113@qq.com>
2021-12-23 15:09:03 +0800

    att no res tcn dp=0.2

--- AGCRN-master/model_old/AGCRN.py
+++ AGCRN-master/model_old/AGCRN.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from model.AGCRNCell2 import AGCRNCell,AGCRNCell2
+from torch.nn.utils import weight_norm
 
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):

987061eba00f682f38495e1203604fc3299e30ac
jc <2428791113@qq.com>
2022-02-22 21:04:03 +0800

    add sp_att

--- AGCRN/model/AGCN.py
+++ AGCRN/model/AGCN.py
@@ -148,7 +148,7 @@ class AVWGCN(nn.Module):
         # supports=torch.einsum("bnn,knm->bknm",score,supports)
         # # print(supports.shape)
         # x_g = torch.einsum("bknm,bmc->bknc", supports, x)      #B, cheb_k, N, dim_in
-        supports=torch.einsum("bnn,bnn->bnn",self.att_score(x),supports)# 加上空间注意力
+        supports=torch.einsum("bnn,bnn->bnn",self.att_score(x)[0],supports)# 加上空间注意力
         x_g = torch.einsum("knm,bmc->bknc", supports, x)      #B, cheb_k, N, dim_in
         x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
         x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out

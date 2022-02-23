346ac40169fdfeb1986a2fae7c32f968e6a2b6de
jc <2428791113@qq.com>
2022-02-23 17:14:32 +0800

    add static A and att

--- AGCRN/model/AGCN.py
+++ AGCRN/model/AGCN.py
@@ -142,6 +142,7 @@ class AVWGCN(nn.Module):
         bias = torch.matmul(node_embeddings, self.bias_pool)                       #N, dim_out
 
         score,_=self.att_score(x) # b n n
+        print(self.sym_norm_Adj_matrix.shape,"aaaa")
         score=torch.einsum("nn,bnn->bnn",self.sym_norm_Adj_matrix,score)
         # # print(score.shape)
         # # print(supports.shape)

9a0bca8fd52cb975d0bebe0f48bc8b5ba17190bc
jc <2428791113@qq.com>
2022-03-18 16:33:35 +0800

    a

--- AGCRN/model/trans_layer.py
+++ AGCRN/model/trans_layer.py
@@ -82,8 +82,8 @@ class Transform(nn.Module):
 
         value = torch.matmul(A, value)
         # value = torch.softmax(value,-2)
-        # value = torch.cat(torch.split(value, x.shape[0], 0), -1).permute(0, 2, 1, 3)
-        value = value.permute(0,2,1,3)
+        value = torch.cat(torch.split(value, x.shape[0], 0), -1).permute(0, 2, 1, 3)
+        # value = value.permute(0,2,1,3)
         # value += x
 
         value = self.ln(value)

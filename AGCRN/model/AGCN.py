d87803a077bf0f5898faa6fd4831e812dcc4131f
jc <2428791113@qq.com>
2022-03-30 18:43:30 +0800

    TA-GCN_linear

--- AGCRN/model/AGCN.py
+++ AGCRN/model/AGCN.py
@@ -167,7 +167,7 @@ class AVWGCN(nn.Module):
         # self.sym_norm_Adj_matrix = F.softmax(self.sym_norm_Adj_matrix, dim=1).to(device=torch.device("cuda"))
         # self.alpha = nn.Parameter(torch.FloatTensor([0.05]), requires_grad=True)  # D
         # self.beta = nn.Parameter(torch.FloatTensor([0.95]), requires_grad=True)  # S
-        # self.Linear=nn.Linear(dim_in,dim_out,bias=True)
+        self.Linear=nn.Linear(dim_in,dim_out,bias=True)
         # self.att_score=Spatial_Attention_layer(adj.shape[0],dim_in,dim_out)
         self.cheb_k = cheb_k
         self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, dim_in, dim_out))
@@ -189,8 +189,8 @@ class AVWGCN(nn.Module):
         for k in range(2, self.cheb_k):
             support_set.append(torch.matmul(2 * supports, support_set[-1]) - support_set[-2])
         supports = torch.stack(support_set, dim=0)
-        weights = torch.einsum('nd,dkio->nkio', node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
-        bias = torch.matmul(node_embeddings, self.bias_pool)                       #N, dim_out
+        # weights = torch.einsum('nd,dkio->nkio', node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
+        # bias = torch.matmul(node_embeddings, self.bias_pool)                       #N, dim_out
 
         # 修改
         # supports=supports+torch.eye(node_num).to(supports.device) # n n
@@ -223,7 +223,9 @@ class AVWGCN(nn.Module):
 
         x_g = torch.einsum("knm,bmc->bknc", supports, x)      #B, cheb_k, N, dim_in
         x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
-        x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
+        x_gconv=self.Linear(x_g)
+        # x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
+
 
         # print(x_gconv.shape)
         # static_out=torch.einsum("nn,bnc->bnc",self.sym_norm_Adj_matrix,x)

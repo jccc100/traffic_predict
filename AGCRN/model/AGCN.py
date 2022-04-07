8284d30ae1562594aaa56c6950df8e9cca3191ec
jc <2428791113@qq.com>
2022-04-07 10:35:34 +0800

    no cheb_k

--- AGCRN/model/AGCN.py
+++ AGCRN/model/AGCN.py
@@ -182,7 +182,8 @@ class AVWGCN(nn.Module):
         # self.linear_b=nn.Parameter(torch.FloatTensor(170,dim_out),requires_grad=True)
         # self.att_score=Spatial_Attention_layer(adj.shape[0],dim_in,dim_out)
         self.cheb_k = cheb_k
-        self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, dim_in, dim_out))
+        # self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, dim_in, dim_out))
+        self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, dim_in, dim_out))
         self.bias_pool = nn.Parameter(torch.FloatTensor(embed_dim, dim_out))
     def forward(self, x, node_embeddings):
         #x shaped[B, N, C], node_embeddings shaped [N, D] -> supports shaped [N, N]
@@ -195,14 +196,15 @@ class AVWGCN(nn.Module):
         node_num = node_embeddings.shape[0]
         # # supports = F.softmax(F.relu(torch.mm(node_embeddings, node_embeddings.transpose(0, 1))+self.att_score(x)[0]), dim=1) # N N
         supports = F.softmax(F.relu(torch.mm(node_embeddings, node_embeddings.transpose(0, 1))), dim=1) # N N
+        supports = torch.eye(node_num).to(supports.device)+ supports
         # #
-        support_set = [torch.eye(node_num).to(supports.device), supports]
-        #default cheb_k = 3
-        for k in range(2, self.cheb_k):
-            print("cheb_kcheb_kcheb_kcheb_kcheb_k")
-            support_set.append(torch.matmul(2 * supports, support_set[-1]) - support_set[-2])
-        supports = torch.stack(support_set, dim=0)
-        weights = torch.einsum('nd,dkio->nkio', node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
+        # support_set = [torch.eye(node_num).to(supports.device), supports]
+        # #default cheb_k = 3
+        # for k in range(2, self.cheb_k):
+        #     print("cheb_kcheb_kcheb_kcheb_kcheb_k")
+        #     support_set.append(torch.matmul(2 * supports, support_set[-1]) - support_set[-2])
+        # supports = torch.stack(support_set, dim=0)
+        weights = torch.einsum('nd,dio->nio', node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
         bias = torch.matmul(node_embeddings, self.bias_pool)                       #N, dim_out
 
         # 修改
@@ -239,10 +241,14 @@ class AVWGCN(nn.Module):
 
         # x_gconv=torch.einsum('bnki,nkio->bno',x_g,self.linear_weight)+self.linear_b # TA-GCN_linear
         # 不改
-        x_g = torch.einsum("knm,bmc->bknc", supports, x)      #B, cheb_k, N, dim_in
-        x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
-        x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
+        # x_g = torch.einsum("knm,bmc->bknc", supports, x)      #B, cheb_k, N, dim_in
+        # x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
+        # x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
 
+        # no cheb_k
+        x_g = torch.einsum("nm,bmc->bnc", supports, x)      #B,  N, dim_in
+        # x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
+        x_gconv = torch.einsum('bni,nio->bno', x_g, weights) + bias     #b, N, dim_out
 
         # print(x_gconv.shape)
         # static_out=torch.einsum("nn,bnc->bnc",self.sym_norm_Adj_matrix,x)

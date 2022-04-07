3f3d547dfb378155e3f19a2c32ab82d937a49cd4
jc <2428791113@qq.com>
2022-04-07 16:14:35 +0800

    test d3

--- AGCRN/model/Run.py
+++ AGCRN/model/Run.py
@@ -152,8 +152,8 @@ def get_adjacent_matrix(distance_file: str, num_nodes: int, id_file: str = None,
 
     return torch.from_numpy(A)
 
-adj=get_adjacent_matrix(distance_file,args.num_nodes)#.to(device=args.device)
-# adj=torch.randn(50,50)
+# adj=get_adjacent_matrix(distance_file,args.num_nodes)#.to(device=args.device)
+adj=torch.randn(50,50)
 #init model
 model = Network(args,adj)
 model = model.to(args.device)

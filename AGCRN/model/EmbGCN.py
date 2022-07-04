b430743df0c19639e2890607f8ec56efd8dad7e2
jc <2428791113@qq.com>
2022-07-04 13:14:27 +0800

    d4 622 nostatic emb10

--- AGCRN/model/EmbGCN.py
+++ AGCRN/model/EmbGCN.py
@@ -133,9 +133,9 @@ class EmbGCN(nn.Module):
     def __init__(self, dim_in, dim_out, adj,cheb_k, embed_dim):
         super(EmbGCN, self).__init__()
         self.cheb_k = cheb_k
-        self.sym_norm_Adj_matrix = torch.from_numpy(sym_norm_Adj(adj)).to(torch.float32).to(torch.device('cuda'))
-        self.sym_norm_Adj_matrix=F.softmax(self.sym_norm_Adj_matrix)
-        self.linear=nn.Linear(dim_in, dim_out,bias=True)
+        # self.sym_norm_Adj_matrix = torch.from_numpy(sym_norm_Adj(adj)).to(torch.float32).to(torch.device('cuda'))
+        # self.sym_norm_Adj_matrix=F.softmax(self.sym_norm_Adj_matrix)
+        # self.linear=nn.Linear(dim_in, dim_out,bias=True)
         # self.sym_norm_Adj_matrix=F.softmax(torch.Tensor(adj).to(torch.device('cuda')))
         # self.SA=Spatial_Attention_layer(adj.shape[0],dim_in,dim_out)
         self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, dim_in, dim_out))
@@ -149,8 +149,8 @@ class EmbGCN(nn.Module):
         support_set = [torch.eye(node_num).to(supports.device), supports]
         supports = torch.stack(support_set, dim=0)
         # 静态邻接矩阵
-        x_static = torch.einsum("nm,bmc->bmc",torch.softmax(self.sym_norm_Adj_matrix,dim=-1),x)
-        x_static = self.linear(x_static)
+        # x_static = torch.einsum("nm,bmc->bmc",torch.softmax(self.sym_norm_Adj_matrix,dim=-1),x)
+        # x_static = self.linear(x_static)
 
 
         weights = torch.einsum('nd,dkio->nkio', node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
@@ -162,8 +162,8 @@ class EmbGCN(nn.Module):
 
         x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
         x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
-        # return x_gconv
-        return x_gconv+torch.sigmoid(x_static)*x_static
+        return x_gconv
+        # return x_gconv+torch.sigmoid(x_static)*x_static
         # return x_gconv+(torch.tanh(x_gconv)+torch.sigmoid(x_static))
 
         # return x_gconv+torch.sigmoid(x_static)*x_static+torch.sigmoid(x_gconv)*x_gconv
